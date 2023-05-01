14e6deeaad4cdf2638e1cfdb2755052b62a343bb
jorgeluis-dev <jorgeluis.geek@gmail.com>
2023-05-01 12:16:37 -0300

    Recognition of cars and people

--- main.py
+++ main.py
@@ -5,17 +5,32 @@ import torch
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
 # Definir as classes de objetos que queremos detectar
-classes = ['person', 'car', 'truck']
+classes = ['Pessoa', 'car', 'carro', 'cachorro']
 
 # Definir as cores para cada classe
 colors = [(0, 255, 0), (255, 0, 0), (0, 0, 255)]
 
-# Inicializar a captura de vídeo da webcam
-cap = cv2.VideoCapture(0)
+# Definir o diretório e o nome do arquivo de vídeo
+video_path = 'videos/dayscarsandpersons.mp4'
+
+# Inicializar a captura de vídeo do arquivo
+cap = cv2.VideoCapture(video_path)
+
+# Definir a largura e a altura desejadas para o vídeo
+width = 1280
+height = 720
+
+# Definir o tamanho do quadro
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
 while True:
     # Ler o próximo quadro do vídeo
     ret, frame = cap.read()
+
+    # Verificar se o quadro foi lido com sucesso
+    if not ret:
+        break
 
     # Detectar objetos no quadro
     results = model(frame, size=640)
@@ -25,11 +40,14 @@ while True:
 
     # Desenhar caixas delimitadoras e rótulos para cada objeto detectado
     for index, obj in objects.iterrows():
-        x1, y1, x2, y2 = obj[['xmin', 'ymin', 'xmax', 'ymax']].values
+        x1, y1, x2, y2 = [int(i) for i in obj[['xmin', 'ymin', 'xmax', 'ymax']].values]
         cls = int(obj['class'])
         if cls < len(classes):
-            cv2.rectangle(frame, (x1, y1), (x2, y2), colors[cls], 2)
-            cv2.putText(frame, classes[cls], (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, colors[cls], 2)
+            color = colors[cls]
+            if len(color) != 3:
+                color = (255, 255, 255)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+            cv2.putText(frame, classes[cls], (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
 
     # Exibir o quadro com os objetos detectados
     cv2.imshow('Object Detection', frame)
